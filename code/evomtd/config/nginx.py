e7111931847c7802a10be717d32a94b55998ab9f
JJ Merelo <jjmerelo@gmail.com>
2020-02-12 13:39:25 +0100

    Adds first file, :see_no_evil:

---
+++ code/evomtd/config/nginx.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+"""
+    This script generates a nginx config file based on the given parameters
+"""
+__author__ = "Ernesto Serrano"
+__license__ = "GPLv3"
+__email__ = "erseco@correo.ugr.es"
+
+from nginx.config.api import Config, Section, Location, EmptyBlock, KeyMultiValueOption
+
+import random
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+gene_ranges = [[512, 2048],   # worker_connections
+               [10, 120],     # keepalive_timeout
+               [0, 1],        # disable_symlinks
+               [0, 1],        # autoindex
+               [0, 1],        # send_timeout
+               [512, 2048],   # large_client_header_buffers
+               [512, 2048],   # client_max_body_size
+               [0, 1],        # server_tokens
+               [0, 1],        # gzip
+               [0, 2],        # X-Frame-Options
+               [0, 5],        # X-Powered-By
+               [0, 1],        # X-Content-Type-Options
+               [0, 2],        # server
+               [0, 2],        # xss
+               [0, 2]]        # content-policy
+
+def generate_random_config():
+    """
+        Generate a random configuration, the positions are the selected NGINX
+        directives to test, we have binary, integer and list directives
+    """
+    config = []
+    for i in range(len(gene_ranges)):
+        config.append( random.randint( gene_ranges[i][0], gene_ranges[i][1] ) )
+    return config
+
+def in_range_change( value, change, gene ):
+    """
+       Changes the value in gene by the quantity change taking into account ranges. Circles back to max or min if these are exceeded
+    """
+
+    new_value = value + change
+#    print( "New value ", new_value, " gene ", gene )
+    if new_value < gene_ranges[gene][0]:
+        return gene_ranges[gene][1] -  ( new_value - gene_ranges[gene][0] + 1 )
+    elif new_value > gene_ranges[gene][1]:
+        return gene_ranges[gene][0] +  new_value - gene_ranges[gene][1] - 1
+    return new_value
+
+def mutate_config( config ):
+    """
+        Selects a single element in the config and generates a "circular" mutation
+        If any of the ranges is exceeded, gets out the other side
+    """
+    gene = random.randint(0, len(config) -1 )
+    change = random.choice( [-1, 1] )
+    new_config = config[:]
+    new_config[gene] = in_range_change( config[gene], change, gene )
+    return new_config
+
+
+def set_directive_on_off(chromosome):
+    return 'on' if chromosome else 'off'
+
+def set_directive_int(chromosome):
+    return chromosome
+
+
+def set_directive_list(chromosome, list):
+
+    try:
+        a = list[chromosome]
+    except e:
+        print("ERRORAZO")
+        print(chromosome)
+        print(list)
+
+    return list[chromosome]
+
+
+def generate(config=generate_random_config()):
+    """
+        Generate a configuration based on the "config" variable, if this var
+        is not setted we generate a random configuration with the function
+        generate_random_config()
+    """
+    events = Section(
+        'events',
+        worker_connections=set_directive_int(config[0])
+    )
+
+    http = Section(
+        'http',
+        include='/etc/nginx/mime.types',
+        default_type='application/octet-stream',
+        access_log='/tmp/nginx-access.log',
+        sendfile='on',
+        keepalive_timeout=set_directive_int(config[1]),
+        disable_symlinks=set_directive_on_off(config[2]),
+        autoindex=set_directive_on_off(config[3]),
+        send_timeout=set_directive_int(config[4]),
+        large_client_header_buffers="%d %d" % (4, set_directive_int(config[5])),
+        client_max_body_size=set_directive_int(config[6]) * 1024,
+        server_tokens=set_directive_on_off(config[7]),
+        gzip=set_directive_on_off(config[8]),
+        log_format='my_tracking $request_body',
+        resolver='8.8.8.8 valid=30s',
+
+    )
+
+    http.sections.add(
+        Section(
+            'server',
+            Location(
+                '^~ /assets/public/assets/',
+                deny='all',
+            ),
+            Location(
+                '^~ /assets/assets/',
+                deny='all',
+            ),
+            Location(
+                '/form',
+                EmptyBlock(access_log=['/tmp/access.log', 'my_tracking']),
+                # duplicate_options('return'=['200']),
+            ),
+            Location(
+                '/',
+                EmptyBlock(add_header=['X-Frame-Options', set_directive_list(config[9], ['SAMEORIGIN', '"ALLOW-FROM http://www.exampletfm.com/"', 'DENY'])]),
+                EmptyBlock(add_header=['X-Powered-By', set_directive_list(config[10], ['PHP/5.3.3', 'PHP/5.6.8', 'PHP/7.2.1', 'Django2.2', 'nginx/1.16.0', 'nginx/1.17.8'])]),
+                EmptyBlock(add_header=['X-Content-Type-Options', set_directive_list(config[11], ['nosniff', '""'])]),
+                EmptyBlock(add_header=['Server', set_directive_list(config[12], ['apache', 'caddy', 'nginx/1.16.0'])]),
+                EmptyBlock(add_header=['X-XSS-Protection', set_directive_list(config[13], ["0", "1", '"1; mode=block"'])]),
+                EmptyBlock(add_header=['Content-Security-Policy',
+                                       set_directive_list(config[14],
+                                                          ["\"default-src 'self'; frame-ancestors 'self';\"", "\"default-src 'none'; frame-ancestors 'none';\"", "\"default-src 'https://*.google.com';\""])]),
+
+                # add_header Content-Security-Policy "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'
+                # root='/var/lib/nginx/html/',
+                root='/tester/site/',
+                index='index.html index.htm',
+                # proxy_pass='http://juice-shop:3000',
+            ),
+            server_name='www.exampletfm.com',
+            listen=80,
+            error_page='500 502 503 504  /50x.html'
+        )
+    )
+
+    nginx = Config(
+        events,
+        http,
+        user='nginx',
+        pid='/var/run/nginx.pid',
+        worker_processes=4,
+        daemon='on',  # passed in Dockerfile CMD
+        error_log='/tmp/nginx-error.log warn',
+    )
+    # print(nginx)
+
+    # Return the generated configuration
+    return nginx
---
+++ code/evomtd/tests/test_generate_nginx_config.py
@@ -0,0 +1,73 @@
+from evomtd.config.nginx import *
+
+"""Pytest TDD Test definition file"""
+__author__ = "Ernesto Serrano"
+__license__ = "GPLv3"
+__email__ = "erseco@correo.ugr.es"
+
+
+def test_set_directive_on_off_bool():
+    """Test directive change"""
+    assert(set_directive_on_off(True) == 'on')
+
+
+def test_set_directive_on_off_int():
+    """Test directive change"""
+    assert(set_directive_on_off(0) == 'off')
+
+
+def test_set_directive_list_value_int():
+    """Test directive change"""
+    chromosome = 1
+    value_list = [1, 2, 3]
+    result = set_directive_list(chromosome, value_list)
+    assert(result == value_list[chromosome])
+
+
+def test_set_directive_list_value_string():
+    """Test directive change"""
+    chromosome = 2
+    value_list = ["test1", "test2", "test3"]
+    result = set_directive_list(chromosome, value_list)
+    assert(not isinstance(result, list))
+    assert(result == value_list[chromosome])
+
+
+def test_generate_config_good():
+    """Test nginx configuration"""
+    config = [200, 39, 0, 1, 0, 1033, 1933, 1, 0, 1, 0, 1, 1, 0, 1, 0]
+    nginx_config = str(generate(config))
+    print(nginx_config)
+    assert("worker_connections 200;" in nginx_config)
+    assert("keepalive_timeout 39;" in nginx_config)
+    assert("disable_symlinks off;" in nginx_config)
+    assert("autoindex on;" in nginx_config)
+    assert("send_timeout 0;" in nginx_config)
+    assert("large_client_header_buffers 4 1033;" in nginx_config)
+    assert("client_max_body_size 1979392;" in nginx_config)
+    assert("server_tokens on;" in nginx_config)
+    assert("gzip off;" in nginx_config)
+    assert('add_header X-Frame-Options "ALLOW-FROM http://www.exampletfm.com/";' in nginx_config)
+    assert("add_header Server caddy;" in nginx_config)
+    assert("add_header X-Content-Type-Options \"\";" in nginx_config)
+    assert("add_header X-Powered-By PHP/5.3.3;" in nginx_config)
+    assert("add_header X-XSS-Protection 0" in nginx_config)
+    assert("add_header Content-Security-Policy \"default-src 'none'; frame-ancestors 'none';\"" in nginx_config)
+
+
+def test_generate_config_bad():
+    """Test nginx configuration"""
+    config = [1, 1, 1, 0, 0, 0, 1933, 1, 0, 1, 0, 1, 1, 0, 1, 0]
+
+    nginx_config = str(generate(config))
+    print(nginx_config)
+    assert("worker_connections 0;" not in nginx_config)
+    assert("keepalive_timeout 0;" not in nginx_config)
+    assert("disable_symlinks off;" not in nginx_config)
+    assert("autoindex on;" not in nginx_config)
+    assert("send_timeout 1;" not in nginx_config)
+    assert("large_client_header_buffers 4 1;" not in nginx_config)
+    assert("add_header Server: Apache;" not in nginx_config)
+    assert("add_header X-Content-Type-Options: '';" not in nginx_config)
+    assert("add_header X-Powered-By: PHP5.6;" not in nginx_config)
+    assert("add_header X-Frame-Options: ALLOW;" not in nginx_config)
